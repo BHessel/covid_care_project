e283e1bfb3b3b8c3a89ae1f14455deaa51c85e2d
BHessel <hesselfitness@gmail.com>
2020-11-06 15:34:47 -0500

    finished seeds

--- covid_care_packages/db/seeds.rb
+++ covid_care_packages/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+10.times do
+    User.create(name: Faker::FunnyName.name,
+                email: Faker::Internet.email,
+                username: Faker::Internet.username)
+end
+
+
+
+item_arr = ['Masks', 'Bottle of Wine', 'Box of Tissues', 'Monopoly', 'Insomnia Cookies', 'Hand Sanitzer', 'Disinfectant Wipes', 'iPhone Charger', 'Flowers', 'Candles', 'Bath Bomb', 'Picture Frame', 'Coloring Book', 'Snuggie', 'Gloves', 'AirPods', 'Dog Treats', 'Manicure Set', 'Puzzle', '$10 Visa Giftcard', 'Slippers']
+
+20.times do
+    Item.create(name: item_arr.sample, 
+                price: rand(5.00..200.00)   
+                description: Faker::Lorem.sentence(word_count: 8))
+end
+
+#how would we assign a product a description, if we weren't using a placeholder?
+
+
+
+
